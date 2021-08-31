1ae3c795e2f4a3213b870f31093daaa4f46cb83c
MeCrazyJunkie <extazzy@gmail.com>
2021-08-31 17:52:59 +0300

    stake anc-ust and mir-ust

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import { LCDClient, MnemonicKey, RawKey } from '@terra-money/terra.js';
-import { Mirror } from '@mirror-protocol/mirror.js';
-import { Anchor, columbus4, AddressProviderFromJson, MARKET_DENOMS, fabricateMarketClaimRewards, fabricateTerraswapSwapANC, queryMarketBorrowerInfo } from '@anchor-protocol/anchor.js'
+import { Mirror, UST } from '@mirror-protocol/mirror.js';
+import { Anchor, columbus4, AddressProviderFromJson, MARKET_DENOMS, fabricateMarketClaimRewards, fabricateTerraswapSwapANC, queryMarketBorrowerInfo, fabricateTerraswapProvideLiquidityANC, fabricateStakingBond } from '@anchor-protocol/anchor.js'
 import { queryTerraswapSimulation } from '@anchor-protocol/anchor.js/dist/queries/terraswap/simulation.js'
+import { queryTerraswapPool } from '@anchor-protocol/anchor.js/dist/queries/terraswap/pool.js'
+
 
 (await import('dotenv')).config()
 
@@ -26,22 +28,82 @@ async function getClaimAndStakeMirrorMsgs(stakeOrSell) {
 
   console.dir(rewards.reward_infos, { depth: null, colors: true });
 
-  const totalRewards = rewards.reward_infos.reduce((carry, item) => carry += Number(item.pending_reward), 0);
-  console.dir(totalRewards, { depth: null, colors: true });
+  const stakingRewards = rewards.reward_infos.reduce((carry, item) => carry += Number(item.pending_reward), 0);
+  console.dir(stakingRewards, { depth: null, colors: true });
+
+  const votingInfo = await mirror.gov.getStaker(key.accAddress);
+  const votingRewards = votingInfo.pending_voting_rewards;
+
+  const totalRewards = Math.floor(Number(stakingRewards) + Number(votingRewards));
+
+  console.dir(votingInfo, { depth: null, colors: true });
+  
 
   const mirrorToken = mirror.assets['MIR'];
+  // console.dir(mirrorToken, { depth: null, colors: true });
+
+  // process.exit();
+
+  const pool = await mirrorToken.pair.getPool();
+  console.dir(pool, { depth: null, colors: true });
+
+  const price = Number(pool.assets[0].amount) / Number(pool.assets[1].amount);
+  // console.dir(price, { depth: null, colors: true });
+
+  const lptokens = Math.floor( totalRewards / Number(pool.assets[1].amount) * Number(pool.total_share) );
+  console.dir(lptokens, { depth: null, colors: true });
+  // process.exit();
+
+  const increaseAllowance = mirrorToken.token.increaseAllowance(mirrorToken.pair.contractAddress, totalRewards);
+
+
+  const provideLiquidityMsg = mirrorToken.pair.provideLiquidity([
+    {
+      info: {
+        token: {
+          contract_addr: mirrorToken.token.contractAddress
+        }
+      },
+      amount: totalRewards.toString()
+    },
+    {
+      info: UST,
+      amount: Math.floor(totalRewards * price).toString()
+    } 
+  ]);
+
+  const stakeLpMsg = mirror.staking.bond(mirrorToken.token.contractAddress, lptokens, mirrorToken.lpToken);
+
+  let msgs = [mirror.staking.withdraw()];
 
 
   if(stakeOrSell == 'stake') {
-    return [mirror.staking.withdraw(), mirror.gov.stakeVotingTokens(mirrorToken.token, totalRewards), mirror.gov.stakeVotingRewards()];
+
+
+    if(votingRewards > 0) {
+      msgs = msgs.concat(mirror.gov.stakeVotingRewards())
+    }
+
+    return [...msgs, ...[mirror.gov.stakeVotingTokens(mirrorToken.token, totalRewards)]]
+    
   }
   else {
-    return [mirror.staking.withdraw(), mirror.gov.withdrawVotingRewards()];
+
+    if(votingRewards > 0) {
+      msgs = msgs.concat(mirror.gov.withdrawVotingRewards())
+    }
+
+
+    return [...msgs, ...[
+      increaseAllowance, 
+      provideLiquidityMsg, 
+      // stakeLpMsg
+    ]];
   }
  
 }
 
-async function getClaimAndSellAnchorMsgs() {
+async function getClaimAndSellAnchorMsgs(stakeOrSell) {
   
   const blockInfo = await lcd.tendermint.blockInfo();
   const info = await queryMarketBorrowerInfo(
@@ -61,13 +123,54 @@ async function getClaimAndSellAnchorMsgs() {
 
   })(addressProvider);
 
-  const sellANCMsg = fabricateTerraswapSwapANC({
-    address: key.accAddress,
-    amount: Math.trunc(info.pending_rewards) / 1000000,
-    to: key.accAddress
-  })(addressProvider);
+  const ANCAmount = Math.trunc(info.pending_rewards) / 1000000;
 
-  return [].concat(claimRewardsMsg, sellANCMsg);
+  if(stakeOrSell == 'sell') {
+
+    const sellANCMsg = fabricateTerraswapSwapANC({
+      address: key.accAddress,
+      amount: ANCAmount,
+      to: key.accAddress
+    })(addressProvider);
+
+    return [].concat(claimRewardsMsg, sellANCMsg);
+  }
+  else {
+
+    const pool = await queryTerraswapPool({
+      lcd: lcd, 
+      pair_contract_address: 'terra1gm5p3ner9x9xpwugn9sp6gvhd0lwrtkyrecdn3'
+    })(addressProvider);
+
+    console.dir(pool, { depth: null, colors: true });
+    
+
+    const price = Number(pool.assets[1].amount) / Number(pool.assets[0].amount); 
+    console.dir(price, { depth: null, colors: true });
+
+    console.dir('ANCAmount ' + ANCAmount, { depth: null, colors: true });
+
+    const lptokens = Number(ANCAmount) / Number(pool.assets[0].amount) * Number(pool.total_share);
+    console.dir(lptokens, { depth: null, colors: true });
+    // process.exit();
+
+    const provideLiquidityANCMsg = fabricateTerraswapProvideLiquidityANC({
+      address: key.accAddress,
+      slippage_tolerance: "0.01",
+      token_amount: ANCAmount,
+      native_amount: ANCAmount * price,
+      quote: 'uusd'
+    })(addressProvider);
+
+    const stakeANC_LP = fabricateStakingBond({
+      address: key.accAddress,
+      amount: lptokens
+    })(addressProvider);
+
+
+
+    return [].concat(claimRewardsMsg, provideLiquidityANCMsg, stakeANC_LP);
+  }
 }
 
 
@@ -75,17 +178,17 @@ async function main() {
 
   let msgs = [];
 
-  const mirrorMsgs = await getClaimAndStakeMirrorMsgs('stake');
+  const mirrorMsgs = await getClaimAndStakeMirrorMsgs('provide');
   msgs = msgs.concat(mirrorMsgs);
 
-  const anchorMsgs = await getClaimAndSellAnchorMsgs();
-  msgs = msgs.concat(anchorMsgs);
+  // const anchorMsgs = await getClaimAndSellAnchorMsgs('stake');
+  // msgs = msgs.concat(anchorMsgs);
 
-  console.dir(msgs, { depth: null, colors: true });
+  console.dir(msgs, { depth: 7, colors: true });
 
   const tx = await wallet.createAndSignTx({
     msgs: msgs,
-    gasPrices: '0.15uusd',
+    gasPrices: '0.456uusd',
     gasAdjustment: 1.25
   });
 
